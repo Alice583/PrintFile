f1501047829c2bde1a73433e31364dd8c34f1e39
Alice <jr.alchemy@gmail.com>
2023-10-03 00:09:23 +0300

    add work

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 #include <fstream>
 
 
+// функция для печати содержимого из файла
+void printFile(std::string filename){
+    std::ifstream filenameForOpen(filename);
+    if (!filenameForOpen){
+        std::wcout << L"Файл не открыт или введен неверно" << "\n";
+    }
+    else{
+        std::wcout << L"Результат:" << "\n";
+        std::string s;
+        while(getline(filenameForOpen, s)){
+            std::cout << s << std::endl;
+        }
+    }
+    filenameForOpen.close();
+}
+
+
+void lines(){}
+
+void tail(){}
+
+void delimiter(){}
+
+
 // функция для проверки введенных значений пользователем и вызовом нужных функций
 void checkCorrectness(int argc, char** argv){
     setlocale(LC_ALL, "rus");
@@ -9,25 +33,21 @@ void checkCorrectness(int argc, char** argv){
         std::wcout << L"Извините, нужно ввести аргументы и имя файла" << "\n";
     }
     else if (argc == 2){
-        std::ifstream filename(argv[argc - 1]);
-            if (!filename){
-            std::wcout << L"Файл не открыт или введен неверно" << "\n";
-            }
-            else{
-                std::wcout << L"Результат:" << "\n";
-                std::string s;
-                while(getline(filename, s)){
-                    std::cout << s << std::endl;
-                }
-            }
-        filename.close();
+        printFile(argv[argc - 1]);
     }
     else{
         for (int i = 1; i < argc - 1; ++i) {
-            std::cout << argv[i] << " ";
+//            std::cout << argv[i];
+            std::string s = "-l";
+            if (argv[i] == s){ std::wcout << L"Оки";}
+            else{
+                std::wcout << L"Извините, такой команды нет.";
+            }
+//            std::cout << argv[i] << " ";
         }
     }
 }
+
 
 int main(int argc, char** argv) {
     checkCorrectness(argc, argv);
