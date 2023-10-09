1076b8869b40ee0451c5832a753864475c35358d
Alice <jr.alchemy@gmail.com>
2023-10-09 11:56:30 +0300

    labwork1 is done

--- main.cpp
+++ main.cpp
@@ -16,30 +16,17 @@ void FromStartToEnd(ArgumentsSTD* argumentsStd) {
     std::ifstream filename_for_open(argumentsStd->filename, std::ios::binary);
     char symb;
     if (!filename_for_open) {
-        std::wcout << L"Файл не открыт или введен неверно" << "\n";
+        std::wcerr << L"Файл не открыт или введен неверно" << "\n";
     } else {
         if (argumentsStd->lines == -1) {
             while (filename_for_open.get(symb) && filename_for_open.good()) {
-                if (argumentsStd->delimiter != '\n' && symb == '\n') {
-                    continue;
-                }
+                std::cout << symb;
+            }
+        } else {
+            int count = 1;
+            while (filename_for_open.get(symb) && filename_for_open.good() && count <= argumentsStd->lines) {
                 std::cout << symb;
                 if (symb == argumentsStd->delimiter) {
-                    if (argumentsStd->delimiter != '\n') {
-                        std::cout << '\n';
-                    }
-                }
-
-            }
-        }
-        else{
-            int count = 1;
-            while (filename_for_open.get(symb) && filename_for_open.good() && count <= argumentsStd->lines){
-                std::cout << symb;
-                if (symb == argumentsStd->delimiter){
-                    if (argumentsStd->delimiter != '\n'){
-                        std::cout << '\n';
-                    }
                     count++;
                 }
             }
@@ -54,7 +41,7 @@ void FromEndToStart(ArgumentsSTD* argumentsStd) {
     char ch;
     std::ifstream filename_for_open(argumentsStd->filename, std::ios::ate | std::ios::binary);
     if (!filename_for_open) {
-        std::wcout << L"Файл не открыт или введен неверно" << "\n";
+        std::wcerr << L"Файл не открыт или введен неверно" << "\n";
     }
     while(count < argumentsStd->lines && filename_for_open.good()) {
         if (filename_for_open.tellg() == 0) {
@@ -68,13 +55,8 @@ void FromEndToStart(ArgumentsSTD* argumentsStd) {
         }
     }
     char symb;
-    filename_for_open.get(symb);
     while (filename_for_open.get(symb) && filename_for_open.good()) {
         std::cout << symb;
-        if (symb == argumentsStd->delimiter) {
-
-        }
-
     }
     filename_for_open.close();
 }
@@ -84,57 +66,50 @@ void FromEndToStart(ArgumentsSTD* argumentsStd) {
 char getDelimiter(char* args){
     if (strlen(args) == 1){
         return args[0];
-    }
-    else if (args[0] == '\\'){
+    } else if (args[0] == '\\'){
         if (args[1] == 'n'){
             return '\n';
-        }
-        else if (args[1] == 't'){
+        } else if (args[1] == 't'){
             return '\t';
-        }
-        else if (args[1] == 'v'){
+        } else if (args[1] == 'v'){
             return '\v';
-        }
-        else if (args[1] == '0'){
+        } else if (args[1] == '0'){
             return '\0';
         }
     }
     if (strlen(args) >= 2){
-        std::wcout << L"Ошибка, неверный делиметр" << '\n';
+        std::wcerr << L"Ошибка, неверный делиметр" << '\n';
     }
     return 0;
 }
 
 
+const char* kLinesStr = "--lines=";
+const char* kDelimiterStr = "--delimiter=";
+const char* kTailStr = "--tail=";
+
+
 ArgumentsSTD CheckCorrectness(int argc, char* argv[], ArgumentsSTD argumentsStd) {
     setlocale(LC_ALL, "rus");
-
     if (argc < 2) {
-        std::wcout << L"Извините, нужно ввести аргументы и имя файла" << "\n";
-    }
-    else {
+        std::wcerr << L"Извините, нужно ввести аргументы и имя файла" << "\n";
+    } else {
         for (int i = 1; i < argc; i++) {
             if (std::strcmp(argv[i], "-l") == 0){
-                argumentsStd.lines = std::atoll(argv[i + 1]);
+                argumentsStd.lines = std::stoll(argv[i + 1]);
                 i++;
-            }
-            else if (std::strncmp(argv[i], "--lines=", 8) == 0){
-                argumentsStd.lines = std::atoll(argv[i] + 8);
-            }
-            else if (std::strcmp(argv[i], "-t") == 0) {
+            } else if (std::strncmp(argv[i], kLinesStr, strlen(kLinesStr)) == 0){
+                argumentsStd.lines = std::stoll(argv[i] + 8);
+            } else if (std::strcmp(argv[i], "-t") == 0) {
                 argumentsStd.from_tail = true;
-            }
-            else if (std::strncmp(argv[i], "--tail", 6) == 0){
+            } else if (std::strncmp(argv[i], kTailStr, strlen(kTailStr)) == 0){
                 argumentsStd.from_tail = true;
-            }
-            else if  (std::strcmp(argv[i], "-d") == 0) {
+            } else if  (std::strcmp(argv[i], "-d") == 0) {
                 argumentsStd.delimiter = getDelimiter(argv[i + 1]);
                 i++;
-            }
-            else if (std::strncmp(argv[i], "--delimiter=", 12) == 0) {
+            } else if (std::strncmp(argv[i], kDelimiterStr, strlen(kDelimiterStr)) == 0) {
                 argumentsStd.delimiter = getDelimiter(argv[i] + 12);
-            }
-            else {
+            } else {
                 argumentsStd.filename = argv[i];
             }
 
